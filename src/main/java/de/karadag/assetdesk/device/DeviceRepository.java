9f12d943b97da003c47cfd95389c12b0df5af70c
Pasa Can Karadag <pasacankaradag91@gmail.com>
2026-09-07 16:51:32 +0200

    Device-Repository ergänzt

--- src/main/java/de/karadag/assetdesk/device/DeviceRepository.java
+++ src/main/java/de/karadag/assetdesk/device/DeviceRepository.java
@@ -1,4 +1,13 @@
 package de.karadag.assetdesk.device;
 
-public class DeviceRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+public  interface DeviceRepository  extends JpaRepository<Device,Long> {
+
+   List<Device> findByStatus(DeviceStatus deviceStatus);
+   boolean existsByInventoryNumber(String inventoryNumber);
+
 }
